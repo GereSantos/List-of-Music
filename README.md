d3d7bd2b718c4068788462a69d756e2929cc3a4d
Geremias Santos <Geremias99999@gmail.com>
2022-08-19 14:42:46 -0300

    List of music

---
+++ README.md
@@ -0,0 +1,8 @@
+
+## Sobre o Mini- Projeto
+
+Programa que executa a procura de um album existente dentro de um objeto(lista de musica), há condicionais aonde retornam o valor caso exista o album,checam se o album existe também faz parte da função em JS.
+
+## Linguagens Utilizadas
+
+- JavaScript
---
+++ script.js
@@ -0,0 +1,37 @@
+//Objeto aonde contém o album de musicas
+const recordCollection = {
+    2548: {
+      albumTitle: 'Slippery When Wet',
+      artist: 'Bon Jovi',
+      tracks: ['Let It Rock', 'You Give Love a Bad Name']
+    },
+    2468: {
+      albumTitle: '1999',
+      artist: 'Prince',
+      tracks: ['1999', 'Little Red Corvette']
+    },
+    1245: {
+      artist: 'Robert Palmer',
+      tracks: []
+    },
+    5439: {
+      albumTitle: 'ABBA Gold'
+    }
+  };
+  
+  //Função que executa a procura de coleção das musicas e retorna ao final de acordo com a condicional
+  function updateRecords(records, id, prop, value) {
+    if (prop !== 'tracks' && value !== "") {
+      records[id][prop] = value;
+    } else if (prop === "tracks" && records[id].hasOwnProperty("tracks") === false) {
+      records[id][prop] = [value];
+    } else if (prop === "tracks" && value !== "") {
+      records[id][prop].push(value);
+    } else if (value === "") {
+      delete records[id][prop];
+    }
+    return records;
+  }
+  
+  //Exemplo de execução
+  updateRecords(recordCollection, 5439, 'artist', 'ABBA');
